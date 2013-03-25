395ad9ffee2cb754eeeec8b2f2b7e94de84f3237
Paul Smith <paulsmith@pobox.com>
2013-03-25 17:39:17 -0400

    Simplify implementation with struct keys

--- context.go
+++ context.go
@@ -10,9 +10,14 @@ import (
 	"time"
 )
 
+type Ctxt struct {
+    req *http.Request
+    key interface{}
+}
+
 var (
 	mutex sync.Mutex
-	data  = make(map[*http.Request]map[interface{}]interface{})
+	data  = make(map[Ctxt]interface{})
 	datat = make(map[*http.Request]int64)
 )
 
@@ -20,30 +25,24 @@ var (
 func Set(r *http.Request, key, val interface{}) {
 	mutex.Lock()
 	defer mutex.Unlock()
-	if data[r] == nil {
-		data[r] = make(map[interface{}]interface{})
-		datat[r] = time.Now().Unix()
-	}
-	data[r][key] = val
+    data[Ctxt{r, key}] = val
+    if _, ok := datat[r]; !ok {
+        datat[r] = time.Now().Unix()
+    }
 }
 
 // Get returns a value stored for a given key in a given request.
 func Get(r *http.Request, key interface{}) interface{} {
 	mutex.Lock()
 	defer mutex.Unlock()
-	if data[r] != nil {
-		return data[r][key]
-	}
-	return nil
+    return data[Ctxt{r, key}]
 }
 
 // Delete removes a value stored for a given key in a given request.
 func Delete(r *http.Request, key interface{}) {
 	mutex.Lock()
 	defer mutex.Unlock()
-	if data[r] != nil {
-		delete(data[r], key)
-	}
+    delete(data, Ctxt{r, key})
 }
 
 // Clear removes all values stored for a given request.
@@ -58,7 +57,11 @@ func Clear(r *http.Request) {
 
 // clear is Clear without the lock.
 func clear(r *http.Request) {
-	delete(data, r)
+    for ctxt, _ := range data {
+        if ctxt.req == r {
+            delete(data, ctxt)
+        }
+    }
 	delete(datat, r)
 }
 
@@ -77,13 +80,13 @@ func Purge(maxAge int) int {
 	count := 0
 	if maxAge <= 0 {
 		count = len(data)
-		data = make(map[*http.Request]map[interface{}]interface{})
+        data = make(map[Ctxt]interface{})
 		datat = make(map[*http.Request]int64)
 	} else {
 		min := time.Now().Unix() - int64(maxAge)
-		for r, _ := range data {
-			if datat[r] < min {
-				clear(r)
+		for ctxt, _ := range data {
+			if datat[ctxt.req] < min {
+				clear(ctxt.req)
 				count++
 			}
 		}
